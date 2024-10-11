c678b7a61c6c1215b565a387d7aeec189a691a08
Quaha <ArtemRomanov2017@yandex.ru>
2024-10-11 20:57:48 +0300

    Completed implementation

--- include/Stack.h
+++ include/Stack.h
@@ -7,27 +7,27 @@ private:
 	StackType* curr_stack = nullptr; // curr_capacity
 	StackType* next_stack = nullptr; // curr_capacity * 2
 
-	int sz = 0;
 	int curr_capacity = 2;
 
-	int lup = 0; // last unpushed prev
-	int lun = 0; // last unpushed next
+	int prev_sz = 0;
+	int curr_sz = 1; // there is a fake element in order to simplify the implementation
+	int next_sz = 0;
 
-	static void destruct(Stack* stack) {
+	void destruct(Stack* stack) {
 		delete[] stack->prev_stack;
 		delete[] stack->curr_stack;
 		delete[] stack->next_stack;
 	}
 
-	static void construct(Stack* stack) {
-		stack->prev_stack = new StackType[1];
-		stack->curr_stack = new StackType[2];
-		stack->next_stack = new StackType[4];
+	void construct(Stack* stack) {
+		stack->prev_stack = new StackType[1]();
+		stack->curr_stack = new StackType[2]();
+		stack->next_stack = new StackType[4]();
 
-		stack->sz = 0;
+		stack->curr_sz = 1;
 		stack->curr_capacity = 2;
-		stack->lup = 0;
-		stack->lun = 0;
+		stack->prev_sz = 0;
+		stack->next_sz = 0;
 	}
 
 public:
@@ -41,40 +41,152 @@ public:
 	}
 
 	Stack(const Stack& other) {
+		this->curr_capacity = other.curr_capacity;
 
+		this->prev_stack = new StackType[curr_capacity / 2];
+		this->prev_stack = new StackType[curr_capacity];
+		this->prev_stack = new StackType[curr_capacity * 2];
+
+		this->curr_sz = other.curr_sz;
+		this->prev_sz = other.prev_sz;
+		this->next_sz = other.next_sz;
+
+		for (int i = 0; i < prev_sz; ++i) {
+			this->prev_stack = other.prev_stack[i];
+		}
+
+		for (int i = 0; i < curr_sz; ++i) {
+			this->curr_stack = other.curr_stack[i];
+		}
+
+		for (int i = 0; i < next_sz; ++i) {
+			this->next_stack = other.next_stack[i];
+		}
 	}
 
 	Stack(Stack&& other) {
+		this->prev_stack = other.prev_stack;
+		this->curr_stack = other.curr_stack;
+		this->next_stack = other.next_stack;
 
+		this->curr_capacity = other.curr_capacity;
+
+		this->curr_sz = other.curr_sz;
+		this->prev_sz = other.prev_sz;
+		this->next_sz = other.next_sz;
+
+		construct(other);
 	}
 
 	Stack& operator=(const Stack& other) {
 		if (this == &other) return;
 
+		this->curr_capacity = other.curr_capacity;
+
+		this->prev_stack = new StackType[curr_capacity / 2];
+		this->prev_stack = new StackType[curr_capacity];
+		this->prev_stack = new StackType[curr_capacity * 2];
+
+		this->curr_sz = other.curr_sz;
+		this->prev_sz = other.prev_sz;
+		this->next_sz = other.next_sz;
+
+		for (int i = 0; i < prev_sz; ++i) {
+			this->prev_stack = other.prev_stack[i];
+		}
+
+		for (int i = 0; i < curr_sz; ++i) {
+			this->curr_stack = other.curr_stack[i];
+		}
+
+		for (int i = 0; i < next_sz; ++i) {
+			this->next_stack = other.next_stack[i];
+		}
 	}
 
 	Stack& operator=(Stack&& other) {
 		if (this == &other) return;
 
+		destruct(this);
+
+		this->prev_stack = other.prev_stack;
+		this->curr_stack = other.curr_stack;
+		this->next_stack = other.next_stack;
+
+		this->curr_capacity = other.curr_capacity;
+
+		this->curr_sz = other.curr_sz;
+		this->prev_sz = other.prev_sz;
+		this->next_sz = other.next_sz;
+
+		construct(other);
 	}
 
 	void push_back(const StackType& value) {
+		curr_stack[curr_sz] = value;
+		++curr_sz;
+		
+		next_stack[next_sz] = curr_stack[next_sz];
+		next_stack[next_sz + 1] = curr_stack[next_sz + 1];
 
+		next_sz += 2;
+
+		if (curr_sz == curr_capacity) {
+			delete[] prev_stack;
+
+			prev_stack = curr_stack;
+			curr_stack = next_stack;
+
+			curr_capacity *= 2;
+			next_stack = new StackType[curr_capacity * 2];
+
+			prev_sz = 0;
+			next_sz = 0;
+		}
 	}
 
-	StackType pop_back() {
+	void pop_back() {
+		if (empty()) {
+			throw std::out_of_range("It is forbidden to use the method .pop_back() on an empty stack!");
+		}
 
+		--curr_sz;
+
+		if (next_sz != 0) {
+			next_sz -= 2;
+		}
+		else {
+			prev_stack[prev_sz] = curr_stack[prev_sz];
+			++prev_sz;
+		}
+
+		if (curr_sz == curr_capacity / 4) {
+			delete[] next_stack;
+
+			next_stack = curr_stack;
+			curr_stack = prev_stack;
+
+			curr_capacity /= 2;
+
+			prev_stack = new StackType[curr_capacity / 2];
+
+			prev_sz = 0;
+			next_sz = 0;
+		}
 	}
 
 	StackType back() const {
-
+		if (empty()) {
+			throw std::out_of_range("It is forbidden to use the method .back() on an empty stack!");
+		}
+		return curr_stack[curr_sz - 1];
 	}
 
 	size_t size() const {
-		return sz;
+		return curr_sz - 1;
 	}
 
 	bool empty() const {
-		return sz == 0;
+		return curr_sz == 1;
 	}
 };
--- test/test_Stack.cpp
+++ test/test_Stack.cpp
@@ -2,6 +2,52 @@
 
 #include <gtest.h>
 
+// constructor
 TEST(Stack, can_create_stack) {
 	ASSERT_NO_THROW(Stack<int> stack);
+}
+
+// push_back, pop_back and back
+TEST(Stack, correct_push_back_working) {
+	Stack<int> stack;
+
+	for (int i = 0; i < 100; i++) {
+		stack.push_back(i);
+		EXPECT_EQ(stack.back(), i);
+	}
+}
+
+TEST(Stack, correct_pop_back_working) {
+	Stack<int> stack;
+
+	for (int i = 0; i < 100; i++) {
+		stack.push_back(i);
+	}
+
+	for (int i = 99; i >= 0; i--) {
+		EXPECT_EQ(stack.back(), i);
+		stack.pop_back();
+	}
+}
+
+TEST(Stack, combination_of_push_and_pop_1) {
+	Stack<int> stack;
+
+	for (int i = 0; i < 100; i++) {
+		stack.push_back(i);
+	}
+
+	for (int i = 0; i < 100; i++) {
+		stack.pop_back();
+	}
+
+	for (int i = 0; i < 100; i++) {
+		stack.push_back(i);
+		EXPECT_EQ(stack.back(), i);
+	}
+
+	for (int i = 99; i >= 0; i--) {
+		EXPECT_EQ(stack.back(), i);
+		stack.pop_back();
+	}
 }
